a9dbcb72b31d2dded10da878ea5dc56798853bde
MizozPL <dox44@wp.pl>
2023-05-27 12:58:24 +0200

    Change ViewLecturerScreen to use LecturerType

--- src/types/lecturer.ts
+++ src/types/lecturer.ts
@@ -5,16 +5,19 @@ export type LecturerType = {
   image: string;
   description: string;
   classes: string[];
-  gradeDistribution: {
-    s_2: number;
-    s_3: number;
-    s_3_5: number;
-    s_4: number;
-    s_4_5: number;
-    s_5: number;
-    s_5_5: number;
-  };
+  gradeDistribution: GradeDistributionType;
 };
+
+export type GradeDistributionType = {
+  [key: string]: number;
+  s_2: number;
+  s_3: number;
+  s_3_5: number;
+  s_4: number;
+  s_4_5: number;
+  s_5: number;
+  s_5_5: number;
+}
 
 export enum LecturerLevel {
   ENGINEER = "engineer",
--- src/views/ViewLecturerScreen.tsx
+++ src/views/ViewLecturerScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import {
   StyleSheet,
   View,
@@ -6,51 +6,66 @@ import {
   Image,
   Dimensions,
   ScrollView,
-  StatusBar,
 } from "react-native";
 import { MainNavigationProps } from "../layout/main/MainLayout";
 import * as Progress from "react-native-progress";
-import { LecturerContext } from "../context/lecturer";
+import { LecturerLevel, LecturerType } from "../types/lecturer";
 
-const lecturerName = "Lecturer Name";
-const lecturerExperience = 0.9;
-const dataItems = ["Item1", "Item2", "Item3", "Item4"];
-const gradesDistribution = [
-  { grade: 2, value: 0.3 },
-  { grade: 3, value: 0.5 },
-  { grade: 3.5, value: 0.7 },
-  { grade: 4, value: 0.8 },
-  { grade: 4.5, value: 0.9 },
-  { grade: 5, value: 1 },
-  { grade: 5.5, value: 0.6 },
-];
+const currentLecturer: LecturerType = {
+  _id: "1234",
+  name: "Maciej Gębala",
+  level: LecturerLevel.DOCTOR,
+  image: "https://storage.googleapis.com/pokedex_photos/53f201ea-f6f1-11ed-abbc-833fa3c660ed.jpeg",
+  description: "Jak jest doktor każdy widzi",
+  classes: [],
+  gradeDistribution: {
+    s_2: 0,
+    s_3: 0,
+    s_3_5: 0,
+    s_4: 0,
+    s_4_5: 0,
+    s_5: 0,
+    s_5_5: 0,
+  },
+};
+
+const LecturerLevelProgress = {
+  [LecturerLevel.ENGINEER]: 0.25,
+  [LecturerLevel.MASTER]: 0.5,
+  [LecturerLevel.DOCTOR]: 0.75,
+  [LecturerLevel.PROFESSOR]: 1,
+};
 
 const screenWidth = Dimensions.get("window").width;
-const imageUrl = "../../assets/gebala_portret.jpg";
 
 export default function ViewLecturerScreen({
   navigation,
 }: MainNavigationProps) {
-  const { lecturer } = useContext(LecturerContext);
+
+  const gradeDistributionItems = Object.keys(currentLecturer.gradeDistribution).map(key => {
+    return { grade: key, value: currentLecturer.gradeDistribution[key] };
+  });
+
+  const totalValue = Math.max(gradeDistributionItems.reduce((acc, item) => acc + item.value, 0), 1);
 
   return (
     <View style={styles.container}>
-      <Image style={styles.image} source={require(imageUrl)} />
+      <Image style={styles.image} source={{ uri: currentLecturer.image}} />
       <ScrollView
         style={styles.scrollContainer}
         contentContainerStyle={styles.scrollContainerContent}
       >
         <View style={styles.spaceForImage} />
-        <Text style={styles.name}>{lecturerName}</Text>
+        <Text style={styles.name}>{currentLecturer.name}</Text>
         <Progress.Bar
-          progress={lecturerExperience}
+          progress={LecturerLevelProgress[currentLecturer.level]}
           width={screenWidth * 0.9}
           height={10}
         />
-        <Text style={styles.heading}>Item List</Text>
+        <Text style={styles.heading}>Classes</Text>
         <View>
-          {dataItems.map((item) => (
-            <Text key={item} style={styles.item}>
+          {currentLecturer.classes.map((item, index) => (
+            <Text key={index} style={styles.item}>
               {"\u2022 "}
               {item}
             </Text>
@@ -58,23 +73,24 @@ export default function ViewLecturerScreen({
         </View>
         <Text style={styles.heading}>Grade Distribution</Text>
         <View>
-          {gradesDistribution.map((item) => (
-            <View key={item.grade} style={styles.gradeRow}>
-              <View style={styles.gradeBubble}>
-                <Text style={styles.gradeText}>{item.grade}</Text>
-              </View>
-              <Progress.Bar
-                progress={item.value}
-                width={0.6 * screenWidth}
-                height={10}
-              />
+        {gradeDistributionItems.map((item, index) => (
+          <View key={index} style={styles.gradeRow}>
+            <View style={styles.gradeBubble}>
+              <Text style={styles.gradeText}>{item.grade.replace("s_", "")}</Text>
             </View>
-          ))}
+            <Progress.Bar
+              progress={item.value / totalValue}
+              width={0.6 * screenWidth}
+              height={10}
+            />
+          </View>
+        ))}
         </View>
       </ScrollView>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
