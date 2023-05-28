b05b89973a09da89c3cdc33a0fdadadb8b5c2464
Xgreg2001 <83090501+Xgreg2001@users.noreply.github.com>
2023-05-28 10:23:48 +0200

    Lecturer View styles

--- src/types/colors.ts
+++ src/types/colors.ts
@@ -1,8 +1,17 @@
 export const COLORS = {
-    tabIconDefault: '#e3e3e3',
-    tabIconSelected: '#89ddff',
     tabBar: '#2d2f31',
-    background: '#1f1f1f',
+    background: '#1B1B1B',
     surface: '#272a2d',
     text: "#e3e3e3",
+    textHint: "#AAAAAA",
+    textHeader: '#f5f5f5',
+    primary: "#6495ED",
+    primaryLight: "#92B1F8",
+    primaryDark: "#4871B3",
+    secondary: "#FFA07A",
+    secondaryLight: "#FFBFA3",
+    secondaryDark: "#C88065",
+    accent: "#00FFFF",
 }
+
+export const GRADE_COLORS = ["#d43a48", "#ffa625", "#0192eb", "#8fb0c4", "#388d3c", "#f0cf98", "#9747ff"]
