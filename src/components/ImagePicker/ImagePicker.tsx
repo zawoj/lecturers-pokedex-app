8455160c8757f980b4b440b30b5487d56241f876
Xgreg2001 <83090501+Xgreg2001@users.noreply.github.com>
2023-05-28 13:12:36 +0200

    Styled ImagePicker

--- src/components/ImagePicker/ImagePicker.tsx
+++ src/components/ImagePicker/ImagePicker.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { Button, Image, View, Platform } from "react-native";
+import { Button, Image, View, Platform, StyleSheet } from "react-native";
 import * as ImagePicker from "expo-image-picker";
+import SimpleButton from "../Buttons/SimpleButton";
+import { COLORS } from "../../types/colors";
 
 export default function ImagePickerComponent({ onChange }: { onChange: any }) {
   const [image, setImage] = useState(null);
@@ -20,23 +22,38 @@ export default function ImagePickerComponent({ onChange }: { onChange: any }) {
   };
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "stretch",
-        justifyContent: "center",
-        width: "100%",
-        margin: 0,
-      }}
-    >
-      <Button title='Pick an image' onPress={pickImage}/>
-      {image && (
-        <Image
-          source={{ uri: image }}
-          resizeMode="contain"
-          style={{ width: '100%', height: undefined, aspectRatio: 1, margin: 10, alignSelf: "center" }}
-        />
-      )}
+    <View style={styles.container}>
+      <SimpleButton title='Pick an image' onPress={pickImage} />
+      <View style={styles.surface}>
+        {image && (
+          <Image
+            source={{ uri: image }}
+            resizeMode="contain"
+            style={styles.image}
+          />
+        )}
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
+    margin: 0,
+  },
+  image: {
+    width: '100%',
+    height: undefined,
+    aspectRatio: 1,
+  },
+  surface: {
+    margin: 10,
+    borderRadius: 10,
+    backgroundColor: COLORS.surface
+
+  }
+});
